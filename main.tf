f7b88f7335eda0eed5688351a47953293bf7b0cc
janehojy <janehojy@gmail.com>
2022-02-05 16:13:22 +0800

    edit test vm name

--- main.tf
+++ main.tf
@@ -203,7 +203,7 @@ resource "null_resource" "configure-cat-app" {
   }
 }
 
-resource "azurerm_virtual_machine" "catapp" {
+resource "azurerm_virtual_machine" "jane-test" {
   name                = "${var.prefix}-test"
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
